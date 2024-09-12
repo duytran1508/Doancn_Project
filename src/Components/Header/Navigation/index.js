3fc995b480302159e1bdaa82427479bc63640b34
duytran1508 <ngocduyxx1508@gmail.com>
2024-09-12 10:51:58 +0700

    cai thien thanh lua chon

--- src/Components/Header/Navigation/index.js
+++ src/Components/Header/Navigation/index.js
@@ -5,13 +5,14 @@ import { Link } from "react-router-dom";
 
 
 
+
 const Navigation = () => {
     return (
       
          <nav>
           <div className="container">
             <div className="row">
-                <div className="col-sm-3 navPart1">
+                <div className="col-sm-2 navPart1">
                     <Button className="allCatTab align-items-center">
                         <span className="icon1 mr-2"><IoIosMenu/></span>
                         <span class= "text">ALL CATEGORIES</span>
@@ -19,15 +20,17 @@ const Navigation = () => {
                     </Button>
                 </div>
 
-                <div className="col-sm-9 navPart2 d-flex align-items-center">
-                    <ul className="list list-inline w-100">
+                <div className="col-sm-10 navPart2 d-flex align-items-center">
+                    <ul className="list list-inline ml-auto">
                         <li className="list-inline-item"><Link to = "/">Home</Link></li>
                         <li className="list-inline-item"><Link to = "/">Fashion</Link></li>
                         <li className="list-inline-item"><Link to = "/">Electronic</Link></li>
                         <li className="list-inline-item"><Link to = "/">Bakery</Link></li>
                         <li className="list-inline-item"><Link to = "/">Grocery</Link></li>
+                        <li className="list-inline-item"><Link to = "/">Mobiles</Link></li>
+                        <li className="list-inline-item"><Link to = "/">Mobiles</Link></li>
                         <li className="list-inline-item"><Link to = "/">Blog</Link></li>
-                        <li className="list-inline-item"><Link to = "/">Contact Us</Link></li>
+                        <li className="list-inline-item"><Link to = "/">Contact</Link></li>
 
                     </ul>
                 </div>
